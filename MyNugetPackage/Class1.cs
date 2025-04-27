beab492bd5904a6adf6658479e55c02555040f85
yuriemori <60845198+yuriemori@users.noreply.github.com>
2025-04-27 09:30:28 +0000

    Add initial implementation of MyNugetPackage with CI/CD workflow for NuGet publishing

---
+++ MyNugetPackage/Class1.cs
@@ -0,0 +1,14 @@
+﻿namespace MyNugetPackage;
+
+public class Class1
+{
+    public string GetMessage()
+    {
+        return $"Hello from MyNugetPackage!This version is {GetVersion()}";
+    }
+    public string GetVersion()
+    {
+        return "1.0.0";
+    }
+
+}
